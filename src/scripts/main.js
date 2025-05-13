78d17560547ea6dc6e2f0e6c3ffc40a0494d8ef9
unknown <vladhafarov@gmail.com>
2025-05-13 20:14:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const thumbsContainer = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  thumbsContainer.addEventListener('click', (evt) => {
+    const target = evt.target;
+    const clickedElement =
+      target.tagName === 'IMG' ? target.parentNode : target;
+
+    if (clickedElement.tagName === 'A' && clickedElement.hasAttribute('href')) {
+      evt.preventDefault();
+
+      const newImageSrc = clickedElement.getAttribute('href');
+
+      largeImg.src = newImageSrc;
+      largeImg.alt = clickedElement.getAttribute('title');
+    }
+  });
+});
